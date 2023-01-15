6b5da67a3e73a19b076023aaf3dc546df8722483
Pranjal Yadav <2019uec1008@mnit.ac.in>
2023-01-16 05:18:21 +0530

    🚀

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import server from "./app";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
